569f3d6862e4963d3750ed2f3e9819d271381d2b
Jonahmallard <jonahmallard@outlook.com>
2020-11-30 17:09:12 -0600

    Refactor promise async/await

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,44 @@ const fetch = require('node-fetch');
 const token = process.env.GITHUB_TOKEN;
 const rootURL = 'https://api.github.com/';
 
-router.get('/', function(req, res, next) {
+// router.get('/', function(req, res, next) {
+//   const username = req.query.username;
+//   // If this is not a "search", just render the index view
+//   if (!username) return res.render('index', {userData: null});
+//   const options = {
+//     header: {
+//       Authorization: `token ${token}`
+//     }
+//   };
+//   let userData;
+//   // For now, we'll pass the token in a querystring
+//   fetch(`${rootURL}users/${username}`, options)
+//     .then(res => res.json())
+//     .then(user => {
+//       userData = user;
+//       return fetch(user.repos_url, options);
+//     })
+//     .then(res => res.json())
+//     .then(repos => {
+//       console.log(repos[0])
+//       userData.repos = repos;
+//       res.render('index', { userData });
+//     })
+// });
+
+router.get('/', async function(req, res, next) {
   const username = req.query.username;
-  // If this is not a "search", just render the index view
   if (!username) return res.render('index', {userData: null});
   const options = {
-    header: {
+    headers: {
       Authorization: `token ${token}`
     }
   };
-  let userData;
-  // For now, we'll pass the token in a querystring
-  fetch(`${rootURL}users/${username}`, options)
-    .then(res => res.json())
-    .then(user => {
-      userData = user;
-      return fetch(user.repos_url, options);
-    })
-    .then(res => res.json())
-    .then(repos => {
-      console.log(repos[0])
-      userData.repos = repos;
-      res.render('index', { userData });
-    })
+  // This is a search for a user
+  const userData = await fetch(`${rootURL}users/${username}`, options).then(res => res.json());
+  const repos = await fetch(userData.repos_url, options).then(res => res.json());
+  userData.repos = repos;
+  res.render('index', { userData });
 });
 
 router.get('/', function(req, res, next) {
